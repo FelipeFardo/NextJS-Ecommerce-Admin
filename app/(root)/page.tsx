1bdf0e5367f8c43a2f694b189934f0f1d4c6af97
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-09-30 23:47:08 -0300

    feat: add Clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button>Clique Me</Button>
+      <UserButton afterSignOutUrl="/">Clique Me</UserButton>
     </div>
   )
 }
